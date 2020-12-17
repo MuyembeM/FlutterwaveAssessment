66a6e7f34531bb7dfb32b22bcc22abdb5c8c773d
MuyembeM <42321758+MuyembeM@users.noreply.github.com>
2020-12-17 23:00:14 +0200

    Update failed_transactions.sql

--- failed_transactions.sql
+++ failed_transactions.sql
@@ -1,1 +1,5 @@
-SELECT merchantid, `status`, chargemessage, createddate, count(id) as numtransactions FROM flutterwave_assessments.transaction_table where createddate >= '2020-10-01' and `status` = 'failed' group by merchantid, status, chargemessage, createddate;
+SELECT merchantid, `status`, chargemessage, createddate, COUNT(id) AS numtransactions 
+FROM flutterwave_assessments.transaction_table 
+WHERE createddate >= '2020-10-01' 
+AND `status` = 'failed' 
+GROUP BY merchantid, status, chargemessage, createddate;
